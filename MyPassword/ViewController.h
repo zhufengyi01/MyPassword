d0854d8123b2ad584adf14f00b86206aa47868cc
朱封毅 <673229963@qq.com>
2016-05-23 09:48:31 +0800

    Initial Commit

---
+++ MyPassword/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPassword
+//
+//  Created by 朱封毅 on 23/05/16.
+//  Copyright © 2016年 taihe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyPassword/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyPassword
+//
+//  Created by 朱封毅 on 23/05/16.
+//  Copyright © 2016年 taihe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
